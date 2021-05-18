48f757bb9de57b5cfc2d919780df57f6786874bb
Aditya Chandeliya <adityakumar@giddh.com>
2021-05-18 11:53:40 +0530

    added project files

---
+++ src/main/java/com/aditya/jaspertest/JasperTestApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.jaspertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasperTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasperTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aditya/jaspertest/JasperTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditya.jaspertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasperTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
